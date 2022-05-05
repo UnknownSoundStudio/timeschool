d8d9393cc8c8ccd881d01214668745f1354b7eef
UnknownSoundStudio <100334994+UnknownSoundStudio@users.noreply.github.com>
2022-05-05 23:29:02 +0900

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,153 @@
+function a() //음악연주: Zoom만 시행하는 과목
+{swal("자세한 기능은 추후 개발예정","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://han.gl/pwifo";
+      break;   
+    default:
+      
+  }
+});
+}
+
+function b() //음악연주: Zoom만 시행하는 과목
+{swal("자세한 기능은 추후 개발예정","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://han.gl/pwifo";
+      break;   
+    default:
+      
+  }
+});
+}
+
+function c() //음악연주: Zoom만 시행하는 과목
+{swal("자세한 기능은 추후 개발예정","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://han.gl/pwifo";
+      break;   
+    default:
+      
+  }
+});
+}
+
+
+function d() //음악연주: Zoom만 시행하는 과목
+{swal("자세한 기능은 추후 개발예정","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://han.gl/pwifo";
+      break;   
+    default:
+      
+  }
+});
+}
+
+function e() //음악연주: Zoom만 시행하는 과목
+{swal("자세한 기능은 추후 개발예정","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://han.gl/pwifo";
+      break;   
+    default:
+      
+  }
+});
+}
+
+function f() //음악연주: Zoom만 시행하는 과목
+{swal("자세한 기능은 추후 개발예정","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://han.gl/pwifo";
+      break;   
+    default:
+      
+  }
+});
+}
+function ss() //탐구 A/B/C: 과목마다 다름
+    {swal("자세한 기능은 추후 개발예정","","info",{
+        buttons: {
+          cancel: "취소",
+          a: { text: "응용 프로그래밍",},
+          b: { text: "빅 데이터",},
+          c: { text: "스마트 앱 개발",},
+          d: { text: "게임 프로그래밍",},
+          e: { text: "자료 구조",},
+        }
+      })
+      .then((value) => {
+        switch (value) {
+       
+          case "a":
+            location.href = "https://han.gl/pwifo";
+            break;
+
+          case "b":
+            location.href = "https://han.gl/pwifo";
+            break;
+
+          case "c":
+            location.href = "https://han.gl/pwifo";
+            break;
+
+          case "d":
+            location.href = "https://han.gl/pwifo";
+            break;
+
+          case "e":
+            location.href = "https://han.gl/pwifo";
+            break;
+       
+          default:
+            
+        }
+      });
+}
